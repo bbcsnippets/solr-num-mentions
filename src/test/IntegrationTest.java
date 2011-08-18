73975832c2649982b16a84c9239086156fade46c
Matt Haynes <matt@matthaynes.net>
2011-08-18 20:52:18 +0100

    Test setup

---
+++ src/test/IntegrationTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class IntegrationTest {
+
+  @Test
+  public void testTheTest() {
+    assertEquals("Result", 50, 50);
+  }
+
+}
